b533a513c9d97589b5f7f462eec07baa82a40037
Anony <ljx1583861@qq.com>
2022-07-14 19:14:14 +0800

    完成

---
+++ 05-模拟实现v-model/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>请输入：<input type="text" :value="name" @keyup="value"/></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "你好！",
+    }
+  },
+  methods: {
+    value(e) {
+      this.name = e.target.value
+    }
+  }
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>请输入：<input type="text" :value="name" /></div>
+  <div>
+    请输入：<input type="text" :value="name" @keyup="debounce" />
+    <h2>{{ name }}</h2>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: "你好！",
+      name: "张三",
+      timer: null,
     }
+  },
+  methods: {
+    debounce(e) {
+      if (this.timer) clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.name = e.target.value
+      }, 200)
+    },
   },
 }
 </script>
