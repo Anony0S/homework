52154dbe539f29b45844cd98c9a7f112ec58e925
Anony <ljx1583861@qq.com>
2022-07-17 18:44:55 +0800

    完成:作业-购物车

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,123 @@
 <template>
-  <div class="box">
-    <dir class="imgBox">
-      <Dog1
-        v-for="(item, index) in arr"
-        :key="index"
-        :item="item"
-        :index="index"
-        v-on:printDog="printDog"
-      ></Dog1>
-    </dir>
-    <div class="text">
-      <ul>
-        <li>喜欢的狗狗</li>
-        <li v-for="(item, index) in loveDog" :key="index">{{ item }}</li>
-      </ul>
-    </div>
+  <div>
+    <table border="1" width="700" style="border-collapse: collapse">
+      <caption>
+        购物车
+      </caption>
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" v-model="value" @change="isAll" />
+            <span>全选</span>
+          </th>
+          <th>名称</th>
+          <th>价格</th>
+          <th>数量</th>
+          <th>总价</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <TheTr
+          v-for="(item, index) in goodList"
+          :key="index"
+          :item="item"
+          :index="index"
+          v-on:addNum="addNum"
+          v-on:sumNum="sumNum"
+          v-on:delList="delList"
+        ></TheTr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>合计:</td>
+          <td colspan="5">{{total}}</td>
+        </tr>
+      </tfoot>
+    </table>
   </div>
 </template>
 
 <script>
-import Dog1 from "./components/Dog1.vue"
+import TheTr from "./components/shopping.vue"
 export default {
   data() {
     return {
-      arr: [
+      goodList: [
         {
-          dogImgUrl:
-            "http://nwzimg.wezhan.cn/contents/sitefiles2029/10146688/images/21129958.jpg",
-          dogName: "博美",
+          name: "诸葛亮",
+          price: 1000,
+          num: 1,
+          checked: true,
         },
         {
-          dogImgUrl:
-            "http://p6.itc.cn/images01/20200716/6f80693e5d584c1c861f5d4c2b8d4004.jpeg",
-          dogName: "泰迪",
+          name: "蔡文姬",
+          price: 1500,
+          num: 1,
+          checked: false,
         },
         {
-          dogImgUrl:
-            "https://th.bing.com/th/id/R.65d3f22071b42bbc27eabbc5c9c10ebf?rik=psfIP0pgeQydkw&pid=ImgRaw&r=0",
-          dogName: "金毛",
+          name: "妲己",
+          price: 2000,
+          num: 1,
+          checked: false,
         },
         {
-          dogImgUrl:
-            "https://pic1.zhimg.com/80/v2-7ba4342e6fedb9c5f3726eb0888867da_1440w.jpg?source=1940ef5c",
-          dogName: "哈士奇",
-        },
-        {
-          dogImgUrl:
-            "https://uploadfile.bizhizu.cn/up/e0/0a/b6/e00ab611e8dead17e19dd01509fdde17.jpg",
-          dogName: "阿拉斯加",
-        },
-        {
-          dogImgUrl:
-            "https://tva2.sinaimg.cn/large/005LOWUxly1h49v8z7c6aj30qn0zktsb.jpg",
-          dogName: "萨摩耶",
+          name: "鲁班",
+          price: 2200,
+          num: 1,
+          checked: false,
         },
       ],
-      loveDog: [],
+      value: null,
+      // total: 0
     }
   },
+  components: { TheTr },
   methods: {
-    printDog(dogName) {
-      this.loveDog.push(dogName)
+    // 数量加
+    addNum(index) {
+      this.goodList[index].num++
+    },
+    // 数量减
+    sumNum(index) {
+      if (this.goodList[index].num > 0) this.goodList[index].num--
+      else return alert("数量不能小于0!")
+    },
+    // 全选
+    isAll() {
+      this.goodList.forEach((item) => (item.checked = this.value))
+    },
+    // 删除
+    delList(index){
+      this.goodList.splice(index, 1)
+    }
+  },
+  watch: {
+    goodList: {
+      deep: true,
+      handler() {
+        // 方法一
+        this.value = this.goodList.find(item => !item.checked) ? false : true; 
+        // 方法二
+        // this.goodList.forEach((item) => {
+        //   if (!item.checked) return (this.value = false)
+        //   else this.value = true
+        // })
+        // 方法三
+        // arr.some(): 返回 true 或者 false, 同every 和 map: 判断 true 和 false 返回新数组 
+      },
     },
   },
-  components: { Dog1 },
+  computed: {
+    total() {
+      return this.goodList.reduce((sum, item) => {
+        sum += item.price * item.num * item.checked
+        return sum
+      }, 0)
+    }
+  }
 }
 </script>
 
-<style>
-* {
-  padding: 0;
-  margin: 0;
-}
-li {
-  list-style: none;
-}
-.box {
-  display: flex;
-  width: 770px;
-  border: 1px solid #000;
-  padding: 0;
-  margin: 10px;
-}
-.imgBox {
-  width: 666px;
-  padding: 0;
-  margin: 0;
-}
-.text {
-  flex: 1;
-  border: 1px solid #000;
-  margin: 10px 10px 10px 0;
-  line-height: 25px;
-  text-align: center;
-}
-li:first-child {
-  border-bottom: 1px solid #000;
-}
-</style>
+<style></style>
