fe18b17729054aea5823ef65216c31bca49e5406
Anony <ljx1583861@qq.com>
2022-07-18 10:34:46 +0800

    封装按钮组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <A></A>
-    <B></B>
+    <MyButton title='成功按钮' type='success' @click="fn"></MyButton>
+    <MyButton title='警告按钮' type='warning' @click="fn2"></MyButton>
+    <MyButton title='蓝色按钮' type='primary'></MyButton>
+    <MyButton title='危险按钮' type='danger'></MyButton>
+    <MyButton title='默认按钮'></MyButton>
   </div>
 </template>
 
 <script>
-import A from "./components/A.vue"
-import B from "./components/B.vue"
+import MyButton from './components/button.vue'
 export default {
-  components: { A, B },
+  components: { MyButton },
+  methods: {
+    fn() {
+      console.log(111);
+    },
+    fn2() {
+      console.log(222);
+    }
+  }
 }
 </script>
 
---
+++ src/components/button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button :class="`one-button-${type}`" @click="click">{{ title }}</button>
+</template>
+
+<script>
+// 需求： 要求 title 和背景颜色自定义
+export default {
+  props: {
+    title: String,
+    type: {
+		type: String,
+		// require: true,
+		default: 'default',
+		// val：type接收到的值
+		validator: (val) => ['danger', 'warning', 'success', 'primary', 'default'].includes(val)
+	}
+  },
+  methods: {
+	click() {
+		this.$emit("click")
+	}
+  }
+}
+</script>
+
+<style scoped>
+button {
+  color: #fff;
+  outline: none;
+  border: none;
+  padding: 8px 24px;
+  border-radius: 4px;
+  font-size: 18px;
+  margin-right: 5px;
+}
+.one-button-default {
+  background-color: rgb(196, 196, 196);
+}
+.one-button-primary {
+  background-color: skyblue;
+}
+.one-button-success {
+  background-color: hotpink;
+}
+.one-button-warning {
+  background-color: yellow;
+}
+.one-button-danger {
+  background-color: red;
+}
+</style>
