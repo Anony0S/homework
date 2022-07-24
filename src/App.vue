b2ac60c41d778db297a970f5e1737772f98d0892
Anony <ljx1583861@qq.com>
2022-07-24 08:12:29 +0800

    作业三级导航

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,10 @@
 <template>
-  <div>
-    <MyHeader title="TabBar案例"></MyHeader>
-    <div style="padding-top: 45px">
-      <router-view></router-view>
-    </div>
-      <MyTabBar :list="tabList" @click="click"></MyTabBar>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import MyHeader from "./components/MyHeader.vue"
-import MyTabBar from "./components/MyTabBar.vue"
-
 export default {
   name: "App",
-  data() {
-    return {
-      comName: "MyGoodsList",
-      tabList: [
-        {
-          iconText: "icon-shangpinliebiao",
-          text: "商品列表",
-          componentName: "MyGoodsList",
-        },
-        {
-          iconText: "icon-sousuo",
-          text: "商品搜索",
-          componentName: "MyGoodsSearch",
-        },
-        {
-          iconText: "icon-user",
-          text: "我的信息",
-          componentName: "MyUserInfo",
-        },
-      ],
-    }
-  },
-  methods: {
-    click(index) {
-      this.comName = this.tabList[index].componentName
-    },
-  },
-  components: { MyHeader, MyTabBar },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,65 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.css"
-import "./assets/fonts/iconfont.css"
-// 导入 aios 组件
-import axios from "axios"
+import Vue from "vue";
+import App from "./App"
 
-// 引入路由模块
-import VueRouter from "vue-router"
-
-import GoodsList from "./views/MyGoodList.vue";
-import GoodsSearch from "./views/MySearch.vue";
-import UserInfo from "./views/MyInfo.vue";
-// 使用路由
-Vue.use(VueRouter)
-// 编写路由规则
-const routes = [
-  {
-    // 默认跳转路径 ==> 重定向
-    path: "/",
-    redirect: "/goodslist"
-  },
-  {
-    path: "/goodslist",
-    component: GoodsList,
-    name: 'MyGoodsList'
-  },
-  {
-    path: "/goodssearch",
-    component: GoodsSearch,
-    name: 'MyGoodsSearch'
-  },
-  {
-    path: "/userinfo",
-    component: UserInfo,
-    name: 'MyUserInfo'
-  },
-  {
-    // 找不到路径的时候
-    path: "*",
-    component: GoodsList
-  },
-]
-
+// 阻止生产提示
 Vue.config.productionTip = false
-// 定义全局指令
-Vue.directive("focus", {
-  inserted(el) {
-    el.focus()
-  }
-})
-
-// 注册路由实例
-const router  = new VueRouter({
-  // 放置路由 规则
-  routes
-})
 
 new Vue({
-  router,
-  render: h => h(App),
-  created() {
-    Vue.prototype.$axios = axios
-  },
-}).$mount('#app')
+	render: h => h(App)
+}).$mount("#app")
